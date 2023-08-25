8105f813bdf15cf1ce48c533b44dc756e867e7a8
khalidh223 <khalidhussain4656@gmail.com>
2023-08-25 18:00:38 -0500

    Initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
-const inter = Inter({ subsets: ['latin'] })
+import type { Metadata } from 'next'
+import ThemeRegistry from './ThemeRegistry';
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +18,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>
+        <ThemeRegistry options={{ key: 'mui' }}>{children}</ThemeRegistry>
+      </body>
     </html>
   )
 }
