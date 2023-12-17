d05c5b018fac7ab447368b3f7b54d5e6d998f41d
khalidh223 <khalidhussain4656@gmail.com>
2023-12-17 00:49:48 -0600

    Prevents loading webapp if on a mobile device, displays mobile ad

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,6 +28,7 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Caveat+Brush&display=swap"
           rel="stylesheet"
         />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
       <body>
         <DrawSocketProvider>
